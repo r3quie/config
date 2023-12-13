d669aea4ac224e71148c5eeb8b39b67db47be45c
r3quie <149444217+r3quie@users.noreply.github.com>
2023-12-13 13:58:31 +0100

    amend -q
    changed grep to -q so it returns nothing, returns if amended

--- copyall.sh
+++ copyall.sh
@@ -14,9 +14,10 @@ cp copyall.sh ~
 #### Gnome to KDE ####
 
 #### AGS ####
-if cat ~/.config/ags/scripts/color_generation/applycolor.sh | grep "sed -i 's/alpha=1/alpha=0.3/' ~/.config/foot/foot.ini"
+if cat ~/.config/ags/scripts/color_generation/applycolor.sh | grep -q "sed -i 's/alpha=1/alpha=0.3/' ~/.config/foot/foot.ini"
 then
     :
 else 
     echo "sed -i 's/alpha=1/alpha=0.3/' ~/.config/foot/foot.ini" >> ~/.config/ags/scripts/color_generation/applycolor.sh
+    echo "amended applycolor.sh - footopa"
 fi
