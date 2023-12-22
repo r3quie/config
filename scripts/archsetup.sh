09e5fff3a73880af1aed2d36a8a4895be5037c55
r3quie <149444217+r3quie@users.noreply.github.com>
2023-12-22 23:13:00 +0100

    Update archsetup.sh

--- scripts/archsetup.sh
+++ scripts/archsetup.sh
@@ -3,18 +3,17 @@
 ###### READ COMMENTS BEFORE RUNNING ######
 ### script assumes logged in sudoer (not root!)
 echo "if you wish to run setup, enter password, press control+c to stop setup"
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@";
 
 # system update + pacman
-pacman -Suy
-pacman -S fish git foot starship
-pacman -S --needed base-devel
+sudo pacman -Suy
+sudo pacman -S fish git foot starship
+sudo pacman -S --needed base-devel
 
 # yay setup
 git clone https://aur.archlinux.org/yay.git
 chown -R "$SUDO_USER":"$SUDO_USER" yay                 #idk if works
 cd yay
-sudo -u "$SUDO_USER" makepkg -si
+sudo makepkg -si
 cd ..
 yay --version
 mkdir gitrepos
@@ -24,7 +23,7 @@ exec ~/gitrepos/config/scripts/setuplinks.sh
 
 # MAKE EFFECTIVE PLS
 exec ~/gitrepos/config/scripts/setuplinks.sh
-sudo -u "$SUDO_USER" yay pfetch
+yay pfetch
 sleep 1
 echo "if you wish to install hyprland + illogical-impulse, continue, press ctrl(+shift)+c and reboot"
 read
