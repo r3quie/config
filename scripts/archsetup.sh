af1987e052e4285ccd8fc3c48e02b381b5ca80ba
r3quie <r3quie@gmail.com>
2023-12-25 21:41:51 +0100

    archsetup var

--- scripts/archsetup.sh
+++ scripts/archsetup.sh
@@ -12,7 +12,7 @@ sudo pacman -S --needed base-devel
 # yay setup
 mkdir gitrepos && cd gitrepos
 git clone https://aur.archlinux.org/yay.git
-chown -R "$SUDO_USER":"$SUDO_USER" yay                 #idk if works
+chown -R "$USER":"$USER" yay                 #idk if works
 cd yay
 sudo makepkg -si
 yay --version
@@ -28,7 +28,7 @@ cd ~/.config/systemd/user
 sudo ln -sf ~/gitrepos/config/scripts/services/startup.service
 
 cd /usr/bin
-sudo ln -sf /home/$SUDO_USER/gitrepos/config/scripts/startup.sh
+sudo ln -sf /home/$USER/gitrepos/config/scripts/startup.sh
 
 sudo systemctl enable script.service
 ######################################################### DOTS
