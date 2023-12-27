f8417fa3dbf374b24298c4e9db81e42d4f4c0173
r3quie <r3quie@gmail.com>
2023-12-27 01:38:30 +0100

    colors and comments

--- scripts/updatedots.sh
+++ scripts/updatedots.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
 
+# colors
+source colors.sh
+
+# check if dots up to date
 cd ~/gitrepos/dots-hyprland
 git remote update
 if git status -uno | grep -q "Your branch is up to date"; then
-	echo "Git dots are up to date"
+	echo -e "${BPurple}Git dots are up to date"
 else
-	echo "Git dots are not up to date"
+	echo -e "${BPurple}Git dots are not up to date"
 fi
 
+# check if ags up to date
 cd ~/gitrepos/ags
 git remote update
 if git status -uno | grep -q "Your branch is up to date"; then
-	echo "Git AGS is up to date"
+	echo -e "${BPurple}Git AGS is up to date"
 else
-	echo "Git AGS is not up to date"
+	echo -e "${BPurple}Git AGS is not up to date"
 fi
 
+# stop for userinput
 echo "press enter to continue with updating illogical-impulse, ctrl(+shift)+c to stop"
 read
 
+# pull ags
 echo "pulling ags"
 cd ~/gitrepos/ags
 git pull origin
+# install
 npm install && meson setup build --reconfigure
 echo "do not forget to sudo"
 meson install -C build
 
+# pull dots
 echo "pulling dots"
 cd ../dots-hyprland
 echo "git pull origin"
 git pull origin
+
+# copy dotfiles
 rm .config/fish/config.fish
 echo "cp -r .config  ~"
 cp -r .config  ~
 echo "cp -r .local ~"
 cp -r .local ~
 
+# amend alpha to colorgeneration on bg change
 if cat ~/.config/ags/scripts/color_generation/applycolor.sh | grep -q "sed -i 's/alpha=1/alpha=0.5/' ~/.config/foot/foot.ini"
 then
     :
@@ -44,6 +56,7 @@ else
     echo "amended applycolor.sh - footopa"
 fi
 
+# just in case, might delete
 cd ~/.config/fish
 rm config.fish
 ln -sf ~/gitrepos/config/.config/fish/config.fish
