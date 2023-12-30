db793c3af28e5acf8b7a756f7da3c8af09e2de95
r3quie <r3quie@gmail.com>
2023-12-30 03:39:56 +0100

    hehe

--- scripts/.fifunctions.sh
+++ scripts/.fifunctions.sh
@@ -43,7 +43,9 @@ fncSetGITDIR
     echo "Enter name of directory with git repos. If empty=gitrepos"
     read gr
 
-    if [ -z "${gr}" ]; then set gr gitrepos
+    if [ -z "$gr" ]; then 
+        set gr gitrepos
+    end
     echo "GITDIR="$gr"" >> GITDIR.sh
     sudo mv GITDIR.sh /etc/profile.d/GITDIR.sh
 end
