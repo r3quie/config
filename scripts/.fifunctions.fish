4c309c631417b4c9cdad7f028d4759dc339cb59b
r3quie <r3quie@gmail.com>
2024-01-03 02:07:41 +0100

    last try fish, I hate you

--- scripts/.fifunctions.fish
+++ scripts/.fifunctions.fish
@@ -1,32 +1,28 @@
 #!/usr/bin/env fish
 
-# setup systemd service for automatic git pulls
-
 function runbashs
     cp ~/scripts/.functions.sh ~/.local/cache/usercstm/
     chmod +x ~/.local/cache/usercstm/.functions.sh
     echo -e "\n$whichfnc" >> ~/.local/cache/usercstm/.functions.sh
-    echo "$whichfnc 2"
-    exec ~/.local/cache/usercstm/.functions.sh && rm ~/.local/cache/usercstm/.functions.sh
+    ~/.local/cache/usercstm/.functions.sh && rm ~/.local/cache/usercstm/.functions.sh
 end
 
 function fncStartup
-    set -l whichfnc fncStartup
+    set -l whichfnc 'fncStartup'
     runbashs
 end
 
 function fncSetuplinks
-    set -l whichfnc fncSetuplinks
+    set -l whichfnc 'fncSetuplinks'
     runbashs
 end
 
 function gitpushconfig
-    set -l whichfnc gitpushconfig
-    echo "$whichfnc 1"
+    set -l whichfnc 'gitpushconfig'
     runbashs
 end
 
 function fncSetGITDIR
-    set -l whichfnc fncSetGITDIR
+    set -l whichfnc 'fncSetGITDIR'
     runbashs
 end
