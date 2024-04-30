1ce55ef72cacdbe21ba5e319d14bb74179eab05c
r3quie <r3quie@gmail.com>
2024-04-30 23:28:45 +0200

    NOT

--- scripts/updatecache.sh
+++ scripts/updatecache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-currentfile = $(python ~/gitrepos/edhcarddealer/edhapi.py)
+currentfile=$(python ~/gitrepos/edhcarddealer/edhapi.py)
 
-if [[ "$(cat ~/gitrepos/edhcarddealer/cache/bulkver.txt)" == "$currentfile" ]]; then
+if [[ "$(cat ~/gitrepos/edhcarddealer/cache/bulkver.txt)" != "$currentfile" ]]; then
   rm ~/gitrepos/edhcarddealer/cache/bulkver.txt
   echo "$currentfile" >> ~/gitrepos/edhcarddealer/cache/bulkver.txt
   wget -c "$currentfile" -O ~/gitrepos/edhcarddealer/cache/oracle-cards.json
