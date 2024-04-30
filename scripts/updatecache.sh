a45d1e0f1eae6d82029d3c2ad07423a1255539bf
r3quie <r3quie@gmail.com>
2024-04-30 23:23:51 +0200

    Crontab job

--- scripts/updatecache.sh
+++ scripts/updatecache.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-wget -c $(python ~/gitrepos/edhcarddealer/edhapi.py) -O ~/gitrepos/edhcarddealer/cache/oracle-cards.json
+currentfile = $(python ~/gitrepos/edhcarddealer/edhapi.py)
+
+if [[ "$(cat ~/gitrepos/edhcarddealer/cache/bulkver.txt)" == "$currentfile" ]]; then
+  rm ~/gitrepos/edhcarddealer/cache/bulkver.txt
+  echo "$currentfile" >> ~/gitrepos/edhcarddealer/cache/bulkver.txt
+  wget -c "$currentfile" -O ~/gitrepos/edhcarddealer/cache/oracle-cards.json
+fi
